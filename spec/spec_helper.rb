a29ca159aa9dc4e2208346f9304fb553edb8fe26
matt.cover <werekraken@gmail.com>
2017-03-03 21:39:30 -0700

    Add DOCKER_BUILD_VERBOSE to toggle watching the image build.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,6 +11,12 @@ RSpec.configure do |config|
   config.add_setting :image, :default => nil
 
   config.before(:suite) do
-    RSpec.configuration.image = Docker::Image.build_from_dir('.', { 'pull' => 1 })
+    RSpec.configuration.image = Docker::Image.build_from_dir('.', { 'pull' => 1 }) do |v|
+      if ENV['DOCKER_BUILD_VERBOSE'] == 'yes'
+        if (log = JSON.parse(v)) && log.key?("stream")
+          $stdout.puts log["stream"]
+        end
+      end
+    end
   end
 end
